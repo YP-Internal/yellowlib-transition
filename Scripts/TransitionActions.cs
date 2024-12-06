69450aa3a28a5bf6bd4fc9ab74e09f7854056eec
RotcivOcnarb <rotciv_ocnarb@hotmail.com>
2024-12-06 12:54:00 -0300

    initial commit

---
+++ Scripts/TransitionActions.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace YellowPanda.Transition {
+    public class TransitionActions : MonoBehaviour {
+        public void Goto(string scene) {
+            Transition.Goto(scene);
+        }
+
+        public void FadeIn() {
+            Transition.FadeIn();
+        }
+    }
+
+}
